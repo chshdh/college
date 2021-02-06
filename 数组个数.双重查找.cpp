d1575aeaa8574f547fb09ab1cfe9f0bdc90de8c9
chengshuo <1132050920@qq.com>
2021-02-06 14:19:40 +0800

    Create 数组个数.双重查找.cpp

---
+++ 数组个数.双重查找.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+	
+
+int main ()
+{
+	int a[] = { 0,1,5,7,2,7 };
+
+	int size = sizeof(a) / sizeof(a[0]);
+	
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = i + 1; j< size; j++)
+			cout << a[i] <<"  "<< a[j] << endl;
+	}
+}
+
+
